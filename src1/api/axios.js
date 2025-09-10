c709083735f14cc8058e21cd6e180add57aa55aa
sowad10 <91271928+sowad10@users.noreply.github.com>
2025-09-10 10:11:56 +0000

    backup changes

--- src1/api/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL:"https://dddp.gov.gh/api",
-    auth: {
-        username: 'msow',
-        password: 'dpGhana@2022'
-    }
-});

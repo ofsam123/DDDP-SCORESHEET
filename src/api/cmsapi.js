f5deba7ed23cb5d528bef14065e5a9ee3c0f030e
ofsam123 <ad12@aoholdings.net>
2025-09-10 10:38:02 +0000

    update

--- src/api/cmsapi.js
+++ src/api/cmsapi.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+const instance = axios.create({
+    // baseURL:"http://localhost:8086/dddp/api/v1/",
      baseURL:"https://dddpadminportal.aoinnovations.org/dddp/api/v1/",
     auth: {
         username: 'admin',
         password: 'sow12345'
     }
 });
+
+export default instance;
